8c73d7732f3eddd408696114f514afbc3666401e
matias-millenaar <matiasmillenaar95@gmail.com>
2022-02-06 16:54:22 -0300

    centered navbar + new favicon icon

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,46 +1,30 @@
-import './NavBar.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+
 
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant='dark' expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                        alt="logo"
-                        src={'./images/logo_argenplugs.png'}
-                        width="37.3"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    ARGENPLUGS
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Plantines</Nav.Link>
-                            <Nav.Link href="#link">Macetas</Nav.Link>
-                            <Nav.Link href="#link">Insumos</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-            </Container>
+            <Navbar.Brand href="#home">
+                <img
+                    alt="logo"
+                    src={'./images/logo_argenplugs.png'}
+                    width="37.3"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                ARGENPLUGS
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
+                <Nav>
+                    <Nav.Link href="#plantines">Plantines</Nav.Link>
+                    <Nav.Link href="#macetas">Macetas</Nav.Link>
+                    <Nav.Link href="#insumos">Insumos</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
 
 export default NavBar
-
-/*
-<Navbar.Brand href="#home">
-<img
-    alt="logo"
-    src={'./images/logo_argenplugs.png'}
-    width="37.3"
-    height="30"
-    className="d-inline-block align-top"
-/>{' '}
-ARGENPLUGS
-</Navbar.Brand>
-*/
