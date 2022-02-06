7511abdb26a243d8b7bbce58414d7523f229ff92
matias-millenaar <matiasmillenaar95@gmail.com>
2022-02-05 21:24:50 -0300

    NavBar Ok

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,46 @@
 import './NavBar.css'
 import Navbar from 'react-bootstrap/Navbar'
-import logo from './public/images/logo_argenplugs.png'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 
 const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-                <img
-                    alt="logo"
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-            React Bootstrap
-            </Navbar.Brand>
+        <Navbar bg="dark" variant='dark' expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt="logo"
+                        src={'./images/logo_argenplugs.png'}
+                        width="37.3"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    ARGENPLUGS
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Plantines</Nav.Link>
+                            <Nav.Link href="#link">Macetas</Nav.Link>
+                            <Nav.Link href="#link">Insumos</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+            </Container>
         </Navbar>
     )
 }
 
 export default NavBar
+
+/*
+<Navbar.Brand href="#home">
+<img
+    alt="logo"
+    src={'./images/logo_argenplugs.png'}
+    width="37.3"
+    height="30"
+    className="d-inline-block align-top"
+/>{' '}
+ARGENPLUGS
+</Navbar.Brand>
+*/
